994f8bfd08debe1c7a604fe6dbff018364815bd9
roknikrol <roknikrol@gmail.com>
2019-09-29 20:05:53 +0300

    Определить кто больше поставил лайков (всего) - мужчины или женщины

---
+++ lesson_6/task_4.sql
@@ -0,0 +1,8 @@
+---- Определить кто больше поставил лайков (всего) - мужчины или женщины?
+   SELECT
+     case when sex = 1 then 'man' else 'woman' end as sex    ----- у меня сгенерировалаось так, что пол это 1 или 0
+    ,SUM((SELECT COUNT(DISTINCT id) FROM likes where user_id = profiles.user_id )) likes_count
+   FROM profiles
+   group by sex
+   order by likes_count DESC
+   LIMIT 1 
