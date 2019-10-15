730ea94572f27fca1da2c4ac56c8e7a71923c0cd
roknikrol <roknikrol@gmail.com>
2019-10-15 22:58:34 +0300

    upd

--- lesson_9/Functions_and_procedures/task_2.sql
+++ lesson_9/Functions_and_procedures/task_2.sql
@@ -1,8 +1,19 @@
-CREATE TRIGGER check_name_descr BEFORE INSERT ON products
+﻿CREATE TRIGGER check_name_descr_insrt BEFORE INSERT ON products
 FOR EACH ROW
 BEGIN
-	SET NEW.name = COALESCE(NEW.name, 'без названия');
-	SET NEW.desription = COALESCE(NEW.desription, 'без названия');
+	IF NEW.name is NULL and NEW.desription is null
+	THEN  
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'UPDATE canceled';
+	END IF;
+END//
+
+CREATE TRIGGER check_name_descr_upd BEFORE UPDATE ON products
+FOR EACH ROW
+BEGIN
+	IF NEW.name is NULL and NEW.desription is null
+	THEN  
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'UPDATE canceled';
+	END IF;
 END//
 
 
